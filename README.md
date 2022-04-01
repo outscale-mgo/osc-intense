ef5c641a2cd2d529d4f758e4f72d7e5dbf61ba70
Matthias Gatto <matthias.gatto@outscale.com>
2022-04-01 10:22:34 +0200

    initial commit
    Signed-off-by: Matthias Gatto <matthias.gatto@outscale.com>

---
+++ README.md
@@ -0,0 +1,1 @@
+### Benchmark Outscale Cloud intensively
---
+++ cloudbench.sh
@@ -0,0 +1,233 @@
+#!/bin/bash
+ponysay benchmark Outscale Instances; sleep 1
+ponysay benchmark Outscale VPC; sleep 1
+ponysay benchmark Outscale Security; sleep 1
+ponysay benchmark Outscale ReadRegions; sleep 1
+ponysay benchmark Outscale Images; sleep 1
+ponysay benchmark Outscale Volume; sleep 1
+ponysay benchmark Outscale Cofee; sleep 1
+ponysay benchmark Outscale I use Arch BTW; sleep 1
+ponysay benchmark Outscale Instances; sleep 1
+ponysay benchmark Outscale VPC; sleep 1
+ponysay benchmark Outscale Security; sleep 1
+ponysay benchmark Outscale ReadRegions; sleep 1
+ponysay benchmark Outscale Images; sleep 1
+ponysay benchmark Outscale Volume; sleep 1
+ponysay benchmark Outscale Cofee; sleep 1
+ponysay benchmark Outscale I use Arch BTW; sleep 1
+ponysay benchmark Outscale Instances; sleep 1
+ponysay benchmark Outscale VPC; sleep 1
+ponysay benchmark Outscale Security; sleep 1
+ponysay benchmark Outscale ReadRegions; sleep 1
+ponysay benchmark Outscale Images; sleep 1
+ponysay benchmark Outscale Volume; sleep 1
+ponysay benchmark Outscale Cofee; sleep 1
+ponysay benchmark Outscale I use Arch BTW; sleep 1
+ponysay benchmark Outscale Instances; sleep 1
+ponysay benchmark Outscale VPC; sleep 1
+ponysay benchmark Outscale Security; sleep 1
+ponysay benchmark Outscale ReadRegions; sleep 1
+ponysay benchmark Outscale Images; sleep 1
+ponysay benchmark Outscale Volume; sleep 1
+ponysay benchmark Outscale Cofee; sleep 1
+ponysay benchmark Outscale I use Arch BTW; sleep 1
+ponysay benchmark Outscale Instances; sleep 1
+ponysay benchmark Outscale VPC; sleep 1
+ponysay benchmark Outscale Security; sleep 1
+ponysay benchmark Outscale ReadRegions; sleep 1
+ponysay benchmark Outscale Images; sleep 1
+ponysay benchmark Outscale Volume; sleep 1
+ponysay benchmark Outscale Cofee; sleep 1
+ponysay benchmark Outscale I use Arch BTW; sleep 1
+ponysay benchmark Outscale Instances; sleep 1
+ponysay benchmark Outscale VPC; sleep 1
+ponysay benchmark Outscale Security; sleep 1
+ponysay benchmark Outscale ReadRegions; sleep 1
+ponysay benchmark Outscale Images; sleep 1
+ponysay benchmark Outscale Volume; sleep 1
+ponysay benchmark Outscale Cofee; sleep 1
+ponysay benchmark Outscale I use Arch BTW; sleep 1
+ponysay benchmark Outscale Instances; sleep 1
+ponysay benchmark Outscale VPC; sleep 1
+ponysay benchmark Outscale Security; sleep 1
+ponysay benchmark Outscale ReadRegions; sleep 1
+ponysay benchmark Outscale Images; sleep 1
+ponysay benchmark Outscale Volume; sleep 1
+ponysay benchmark Outscale Cofee; sleep 1
+ponysay benchmark Outscale I use Arch BTW; sleep 1
+ponysay benchmark Outscale Instances; sleep 1
+ponysay benchmark Outscale VPC; sleep 1
+ponysay benchmark Outscale Security; sleep 1
+ponysay benchmark Outscale ReadRegions; sleep 1
+ponysay benchmark Outscale Images; sleep 1
+ponysay benchmark Outscale Volume; sleep 1
+ponysay benchmark Outscale Cofee; sleep 1
+ponysay benchmark Outscale I use Arch BTW; sleep 1
+ponysay benchmark Outscale Instances; sleep 1
+ponysay benchmark Outscale VPC; sleep 1
+ponysay benchmark Outscale Security; sleep 1
+ponysay benchmark Outscale ReadRegions; sleep 1
+ponysay benchmark Outscale Images; sleep 1
+ponysay benchmark Outscale Volume; sleep 1
+ponysay benchmark Outscale Cofee; sleep 1
+ponysay benchmark Outscale I use Arch BTW; sleep 1
+ponysay benchmark Outscale Instances; sleep 1
+ponysay benchmark Outscale VPC; sleep 1
+ponysay benchmark Outscale Security; sleep 1
+ponysay benchmark Outscale ReadRegions; sleep 1
+ponysay benchmark Outscale Images; sleep 1
+ponysay benchmark Outscale Volume; sleep 1
+ponysay benchmark Outscale Cofee; sleep 1
+ponysay benchmark Outscale I use Arch BTW; sleep 1
+ponysay benchmark Outscale Instances; sleep 1
+ponysay benchmark Outscale VPC; sleep 1
+ponysay benchmark Outscale Security; sleep 1
+ponysay benchmark Outscale ReadRegions; sleep 1
+ponysay benchmark Outscale Images; sleep 1
+ponysay benchmark Outscale Volume; sleep 1
+ponysay benchmark Outscale Cofee; sleep 1
+ponysay benchmark Outscale I use Arch BTW; sleep 1
+ponysay benchmark Outscale Instances; sleep 1
+ponysay benchmark Outscale VPC; sleep 1
+ponysay benchmark Outscale Security; sleep 1
+ponysay benchmark Outscale ReadRegions; sleep 1
+ponysay benchmark Outscale Images; sleep 1
+ponysay benchmark Outscale Volume; sleep 1
+ponysay benchmark Outscale Cofee; sleep 1
+ponysay benchmark Outscale I use Arch BTW; sleep 1
+ponysay benchmark Outscale Instances; sleep 1
+ponysay benchmark Outscale VPC; sleep 1
+ponysay benchmark Outscale Security; sleep 1
+ponysay benchmark Outscale ReadRegions; sleep 1
+ponysay benchmark Outscale Images; sleep 1
+ponysay benchmark Outscale Volume; sleep 1
+ponysay benchmark Outscale Cofee; sleep 1
+ponysay benchmark Outscale I use Arch BTW; sleep 1
+ponysay benchmark Outscale Instances; sleep 1
+ponysay benchmark Outscale VPC; sleep 1
+ponysay benchmark Outscale Security; sleep 1
+ponysay benchmark Outscale ReadRegions; sleep 1
+ponysay benchmark Outscale Images; sleep 1
+ponysay benchmark Outscale Volume; sleep 1
+ponysay benchmark Outscale Cofee; sleep 1
+ponysay benchmark Outscale I use Arch BTW; sleep 1
+ponysay benchmark Outscale Instances; sleep 1
+ponysay benchmark Outscale VPC; sleep 1
+ponysay benchmark Outscale Security; sleep 1
+ponysay benchmark Outscale ReadRegions; sleep 1
+ponysay benchmark Outscale Images; sleep 1
+ponysay benchmark Outscale Volume; sleep 1
+ponysay benchmark Outscale Cofee; sleep 1
+ponysay benchmark Outscale I use Arch BTW; sleep 1
+ponysay benchmark Outscale Instances; sleep 1
+ponysay benchmark Outscale VPC; sleep 1
+ponysay benchmark Outscale Security; sleep 1
+ponysay benchmark Outscale ReadRegions; sleep 1
+ponysay benchmark Outscale Images; sleep 1
+ponysay benchmark Outscale Volume; sleep 1
+ponysay benchmark Outscale Cofee; sleep 1
+ponysay benchmark Outscale I use Arch BTW; sleep 1
+ponysay benchmark Outscale Instances; sleep 1
+ponysay benchmark Outscale VPC; sleep 1
+ponysay benchmark Outscale Security; sleep 1
+ponysay benchmark Outscale ReadRegions; sleep 1
+ponysay benchmark Outscale Images; sleep 1
+ponysay benchmark Outscale Volume; sleep 1
+ponysay benchmark Outscale Cofee; sleep 1
+ponysay benchmark Outscale I use Arch BTW; sleep 1
+ponysay benchmark Outscale Instances; sleep 1
+ponysay benchmark Outscale VPC; sleep 1
+ponysay benchmark Outscale Security; sleep 1
+ponysay benchmark Outscale ReadRegions; sleep 1
+ponysay benchmark Outscale Images; sleep 1
+ponysay benchmark Outscale Volume; sleep 1
+ponysay benchmark Outscale Cofee; sleep 1
+ponysay benchmark Outscale I use Arch BTW; sleep 1
+ponysay benchmark Outscale Instances; sleep 1
+ponysay benchmark Outscale VPC; sleep 1
+ponysay benchmark Outscale Security; sleep 1
+ponysay benchmark Outscale ReadRegions; sleep 1
+ponysay benchmark Outscale Images; sleep 1
+ponysay benchmark Outscale Volume; sleep 1
+ponysay benchmark Outscale Cofee; sleep 1
+ponysay benchmark Outscale I use Arch BTW; sleep 1
+ponysay benchmark Outscale Instances; sleep 1
+ponysay benchmark Outscale VPC; sleep 1
+ponysay benchmark Outscale Security; sleep 1
+ponysay benchmark Outscale ReadRegions; sleep 1
+ponysay benchmark Outscale Images; sleep 1
+ponysay benchmark Outscale Volume; sleep 1
+ponysay benchmark Outscale Cofee; sleep 1
+ponysay benchmark Outscale I use Arch BTW; sleep 1
+ponysay benchmark Outscale Instances; sleep 1
+ponysay benchmark Outscale VPC; sleep 1
+ponysay benchmark Outscale Security; sleep 1
+ponysay benchmark Outscale ReadRegions; sleep 1
+ponysay benchmark Outscale Images; sleep 1
+ponysay benchmark Outscale Volume; sleep 1
+ponysay benchmark Outscale Cofee; sleep 1
+ponysay benchmark Outscale I use Arch BTW; sleep 1
+ponysay benchmark Outscale Instances; sleep 1
+ponysay benchmark Outscale VPC; sleep 1
+ponysay benchmark Outscale Security; sleep 1
+ponysay benchmark Outscale ReadRegions; sleep 1
+ponysay benchmark Outscale Images; sleep 1
+ponysay benchmark Outscale Volume; sleep 1
+ponysay benchmark Outscale Cofee; sleep 1
+ponysay benchmark Outscale I use Arch BTW; sleep 1
+ponysay benchmark Outscale Instances; sleep 1
+ponysay benchmark Outscale VPC; sleep 1
+ponysay benchmark Outscale Security; sleep 1
+ponysay benchmark Outscale ReadRegions; sleep 1
+ponysay benchmark Outscale Images; sleep 1
+ponysay benchmark Outscale Volume; sleep 1
+ponysay benchmark Outscale Cofee; sleep 1
+ponysay benchmark Outscale I use Arch BTW; sleep 1
+ponysay benchmark Outscale Instances; sleep 1
+ponysay benchmark Outscale VPC; sleep 1
+ponysay benchmark Outscale Security; sleep 1
+ponysay benchmark Outscale ReadRegions; sleep 1
+ponysay benchmark Outscale Images; sleep 1
+ponysay benchmark Outscale Volume; sleep 1
+ponysay benchmark Outscale Cofee; sleep 1
+ponysay benchmark Outscale I use Arch BTW; sleep 1
+ponysay benchmark Outscale Instances; sleep 1
+ponysay benchmark Outscale VPC; sleep 1
+ponysay benchmark Outscale Security; sleep 1
+ponysay benchmark Outscale ReadRegions; sleep 1
+ponysay benchmark Outscale Images; sleep 1
+ponysay benchmark Outscale Volume; sleep 1
+ponysay benchmark Outscale Cofee; sleep 1
+ponysay benchmark Outscale I use Arch BTW; sleep 1
+ponysay benchmark Outscale Instances; sleep 1
+ponysay benchmark Outscale VPC; sleep 1
+ponysay benchmark Outscale Security; sleep 1
+ponysay benchmark Outscale ReadRegions; sleep 1
+ponysay benchmark Outscale Images; sleep 1
+ponysay benchmark Outscale Volume; sleep 1
+ponysay benchmark Outscale Cofee; sleep 1
+ponysay benchmark Outscale I use Arch BTW; sleep 1
+ponysay benchmark Outscale Instances; sleep 1
+ponysay benchmark Outscale VPC; sleep 1
+ponysay benchmark Outscale Security; sleep 1
+ponysay benchmark Outscale ReadRegions; sleep 1
+ponysay benchmark Outscale Images; sleep 1
+ponysay benchmark Outscale Volume; sleep 1
+ponysay benchmark Outscale Cofee; sleep 1
+ponysay benchmark Outscale I use Arch BTW; sleep 1
+ponysay benchmark Outscale Instances; sleep 1
+ponysay benchmark Outscale VPC; sleep 1
+ponysay benchmark Outscale Security; sleep 1
+ponysay benchmark Outscale ReadRegions; sleep 1
+ponysay benchmark Outscale Images; sleep 1
+ponysay benchmark Outscale Volume; sleep 1
+ponysay benchmark Outscale Cofee; sleep 1
+ponysay benchmark Outscale I use Arch BTW; sleep 1
+ponysay benchmark Outscale Instances; sleep 1
+ponysay benchmark Outscale VPC; sleep 1
+ponysay benchmark Outscale Security; sleep 1
+ponysay benchmark Outscale ReadRegions; sleep 1
+ponysay benchmark Outscale Images; sleep 1
+ponysay benchmark Outscale Volume; sleep 1
+ponysay benchmark Outscale Cofee; sleep 1
+ponysay benchmark Outscale I use Arch BTW; sleep 1
